5a5a899aca8435cf27d97ace09d48ba524bbad78
jeonjonghyeok <tongher1685@gmail.com>
2021-06-27 04:29:13 +0900

    feat #5 Explorer

---
+++ blockchain/blockchain.go
@@ -0,0 +1,59 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type Block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*Block
+}
+
+//singleton pattern
+//only this package
+var b *blockchain
+var once sync.Once
+
+func (b *Block) calculateHash() string {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	return fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *Block {
+	newBlock := Block{data, "", getLastHash()}
+	newBlock.Hash = newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+		})
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*Block {
+	return b.blocks
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeonjonghyeok/coin
+
+go 1.16
